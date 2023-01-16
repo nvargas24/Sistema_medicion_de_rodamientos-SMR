f37bfe236fbc8eb4133a05053bcc69f66a35c929
Nahuel Vargas <nahuvargas24@gmail.com>
2023-01-15 22:52:20 -0300

    lectura con todos los caracteres del portserial

--- readSerial_simple.py
+++ readSerial_simple.py
@@ -3,15 +3,10 @@ from time import sleep
 
 try:
     ser=serial.Serial('COM3', 115200)
-    #ser.setDTR(False)
     sleep(2)
-    #ser.flushInput()
-    #ser.setDTR(True)
     while True:
         dato=ser.readline()
         print(dato)
-        ser.close()
-    
 except:
     print("Error, saliendo")
-
+    ser.close()
