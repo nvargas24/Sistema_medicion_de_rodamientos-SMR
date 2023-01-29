c055a9b1d954446e05f03b036ba4311b6ef0ebe0
Nahuel Vargas <nahuvargas24@gmail.com>
2023-01-29 15:58:57 -0300

    se filtra datos de puerto serial

--- readSerial_simple.py
+++ readSerial_simple.py
@@ -1,12 +1,21 @@
 import serial
 from time import sleep
 
-try:
-    ser=serial.Serial('COM3', 115200)
-    sleep(2)
-    while True:
-        dato=ser.readline()
-        print(dato)
-except:
-    print("Error, saliendo")
-    ser.close()
+ser=serial.Serial('COM3', 115200, timeout=1, write_timeout=1)
+
+while True:
+    sleep(0.2)
+    dato=ser.readline()
+    dato=dato.decode('utf-8') #se interpresta como caracteres segun la unicode
+
+    print(dato)
+    print(dato.find("{"))
+    print(dato.find("}"))
+
+    if dato.find("}")>=0 and dato.find("{")==0:
+        print("Encontrado")
+        print(dato[1:dato.find("}")])
+    else:
+        print("No encotrado")
+
+    print("-"*30)
