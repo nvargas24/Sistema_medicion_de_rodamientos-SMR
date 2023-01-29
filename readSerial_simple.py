1404063e694b4e05d8a5b15b45a8e3139ddba15a
Nahuel Vargas <nahuvargas24@gmail.com>
2023-01-29 17:09:50 -0300

    recibir en formato json

--- readSerial_simple.py
+++ readSerial_simple.py
@@ -1,21 +1,21 @@
 import serial
+import json
 from time import sleep
 
 ser=serial.Serial('COM3', 115200, timeout=1, write_timeout=1)
 
 while True:
-    sleep(0.2)
-    dato=ser.readline()
-    dato=dato.decode('utf-8') #se interpresta como caracteres segun la unicode
+    try:
+        dato=ser.readline()
+        dato=dato.decode('utf-8') #se interpresta como caracteres segun la unicode
 
-    print(dato)
-    print(dato.find("{"))
-    print(dato.find("}"))
-
-    if dato.find("}")>=0 and dato.find("{")==0:
-        print("Encontrado")
-        print(dato[1:dato.find("}")])
-    else:
-        print("No encotrado")
-
-    print("-"*30)
+        if (dato.find("}")>=0 and dato.find("{")==0):
+            data_serial = dato[0:dato.find("}")+1]
+            json_data = json.loads(data_serial)
+            print(json_data["Type"])
+            print(json_data["Index"])
+            print(json_data["Value"])
+    except:
+        print("Cerrar puerto")
+        ser.close()
+        break
