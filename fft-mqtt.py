bf771e362fe01471576968a75365ca78dda6ea81
Nahuel Vargas <nahuvargas24@gmail.com>
2023-04-09 23:50:06 -0300

    establezco conexion con grafico fft y mqtt

--- fft-mqtt.py
+++ fft-mqtt.py
@@ -4,50 +4,90 @@ import matplotlib.animation as animation
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import style
+import paho.mqtt.client as mqtt
+
+import paho.mqtt.client as mqtt
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 MUESTRAS_FFT = 512
-TIME_SWEEP = 100 #mseconds 
 
-x1=[]
-y1=[]
-
-plt.rcParams['toolbar'] = 'None'
-
-for i in range(MUESTRAS_FFT):
-    x1.append(37*i)
-
-for j in range(MUESTRAS_FFT):
-    y1.append(-15)
-
-root = tkinter.Tk()
-root.wm_title("Grafica test")
-ta=root.geometry("700x420")
-
-style.use("bmh")
-
-fig=Figure(dpi=60)
-graph=fig.add_subplot(111)
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-cont=0
-
-def animate(i):
-    global cont
-    print("actualizar grafica")
-
-    graph.clear() 
-    graph.plot(x1, y1)
-
-# animacion para actualizar datos de grafico
-ani = animation.FuncAnimation(fig, animate, interval=TIME_SWEEP)
-plt.show()
+x=[]
+y=[]
 
 def cerrar():
     root.quit()
 
-button=tkinter.Button(master=root, text="cerrar", command=cerrar)
-button.pack(side=tkinter.BOTTOM)
+def init_fft():
+    client.publish("smr/start", 1)
+    print("Inicia lectura de datos")
 
-tkinter.mainloop()
+def finish_fft():
+    client.publish("smr/stop", 1)
+    plt.clf() # Limpiar gráfico anterior
+    print("Finalizo conexion para recibir datos por mqtt")
+
+# Función que se llama cuando se recibe un mensaje MQTT
+def on_message(client, userdata, msg):
+    global x
+    global y
+
+    message = msg.payload.decode()
+    values_str = message.split(",")
+    y = [float(value) for value in values_str]
+    print("Se actualiza datos de fft")
+    plt.clf() # Limpiar gráfico anterior
+    plt.plot(x, y)
+    plt.title("Sweep FFT")
+    plt.xlabel("Frecuencia[kHz]")
+    plt.ylabel("Amplitud[dBV]")
+    plt.xlim(-100, 19000) # Establecer límites del eje X
+    plt.ylim(-40, 60) # Establecer límites del eje Y
+    canvas.draw() # Actualizar gráfico
+
+#completa array de float para resolucion de frecuencia de 37Hz
+for i in range(MUESTRAS_FFT):
+    x.append(37*i)
+
+# Crear ventana de Tkinter
+root = tk.Tk()
+root.title("Test fft con MQTT")
+root.geometry("500x400")
+style.use("bmh")
+
+# Crear lienzo para el gráfico
+fig, ax = plt.subplots(figsize=(4, 3), dpi=70)
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+# Establecer límites del eje X e Y
+ax.set_xlim(-100, 19000)
+ax.set_ylim(-40, 60)
+#plt.xticks(np.arange(0, 21, step=1))  # Set label locations.
+plt.legend(loc = "upper right")
+plt.title("Sweep FFT")
+plt.xlabel("Frecuencia[kHz]")
+plt.ylabel("Amplitud[dBV]")
+
+#crea botton e ventana principal
+btn_cerrar=tkinter.Button(root, text="Cerrar", command=cerrar)
+btn_finish = tk.Button(root, text="Finalizar conexion", command=finish_fft)
+btn_init = tk.Button(root, text="Iniciar conexion", command=init_fft)
+
+btn_cerrar.pack(side=tkinter.BOTTOM)
+btn_finish.pack(side=tkinter.BOTTOM)
+btn_init.pack(side=tkinter.BOTTOM)
+
+# Conectar al servidor MQTT y suscribirse al tema "/mytopic"
+client = mqtt.Client()
+client.connect("192.168.1.110", 1883)
+client.subscribe("rodAnt/fft")
+client.on_message = on_message
+client.loop_start()
+
+# Iniciar bucle de eventos de Tkinter
+root.mainloop()
