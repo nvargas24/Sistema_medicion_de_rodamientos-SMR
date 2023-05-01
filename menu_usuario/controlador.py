4461055e181f4be65782c37795ad4104993f761c
Nahuel Vargas <nahuvargas24@gmail.com>
2023-05-01 11:43:08 -0300

    controlador y vista base para ejemplo con coment

---
+++ menu_usuario/controlador.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2 import QtCore as core
+
+from vista import *
+from modelo import *
+
+class Controlador():
+    def __init__(self, ):
+        # se deben crear las clases que interactuan con los .ui en vista.py
+        pass
+
+if __name__ == "__main__":
+    # Creao objeto controlador 
+    obj_controlador = Controlador()
+
---
+++ menu_usuario/vista.py
@@ -0,0 +1,34 @@
+"""
+ IMPORTANTE: 
+    NO SE DEBEN MODIFICAR LOS ARCHIVOS DE LA CARPETA QT, YA QUE ESTO UNICAMENTE SE
+ MODIFICAN DESDE LA APP QTDESIGNER, SINO AL REALIZAR UN MINIMO CAMBIO EN QTDESIGNER,
+ EL CODIGO ESCRITO SE BORRARA. SI SE QUIERE HACER ALGUN CAMBIO POR CODIGO, SE REALIZAN 
+ ACA QUE INTERACTURA CON LAS CLASES Y METODOS DE OBTENIDOS CON PYSIDE2 DE LOS .UI
+"""
+import os
+import sys
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2 import QtCore as core
+
+# Se importa libreria de archivos .py creados con pyside2 de un .ui
+# Necesario para acceder a las clases y metodos de cada widget en .ui
+from Qt.window_main import *
+from Qt.window_usuario import *
+
+# Hay que tener en cuenta la clase definida en el qtdesigner de cada ventana
+# Esto se puede editar en QtDesigner
+class Mainwindwow(QMainWindow):
+    def __init__(self, ):
+        super().__init__() # Accedo constructor de QMainWindwow, por si se cambia de nombre es mejor utilizar super()
+        self.ui = UiMainWindow() # Instancio la ventana main
+        self.ui.setupUi(self) # Se pasa objeto de la clase MainWindow asi se
+                              # se identifica el espacio de trabajo
+                              # En el archivo window_main.py se puede ver el nombre que se lo 
+                              # identifica al objeto, esto se puede modificar en QtDesigner
+        # Ahora puedo acceder a los metodos de cada widget creado por medio del objeto self .ui
+        
+
+
+
