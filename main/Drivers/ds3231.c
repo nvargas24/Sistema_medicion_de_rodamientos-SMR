0dd35c7099ca99a9f8b2c7076d0c7887bb9fdc3f
Nahuel Vargas <nahuvargas24@gmail.com>
2023-08-19 19:24:47 -0300

    timestamp en segundos + conversion hex2dec

--- main/Drivers/ds3231.c
+++ main/Drivers/ds3231.c
@@ -66,17 +66,56 @@ esp_err_t DS3231_GetTime(uint8_t *time)
     return ret;
 }
 
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int is_leap_year(int year)
+{
+    return (year%4 == 0 && (year%100 != 0 || year%400 == 0));
+}
+
+
+uint8_t hexToDecimal(uint8_t hexValue)
+{   
+    uint8_t decimalValue = ((hexValue >>4)*10) + (hexValue & 0x0F);
+    return decimalValue;
+}
+
 esp_err_t DS3231_GetTimeStamp(char *timestamp)
 {
     esp_err_t ret;
     uint8_t time[7];
+    int timestamp_segs = 0;
+    
 
     ret = DS3231_GetTime(&time);
     if(ret == ESP_OK)
     {
-        /* AA_MM_DD_HH_MM_SS */
-        sprintf(timestamp, "20%d:%d:%d %d:%d:%d", time[7], time[6], time[5], time[3], time[2],time[1]);
+        
+        int total_days = 0;
+
+        for(int year=2000; year<hexToDecimal(time[6]) + 1900; year++)
+        {
+            total_days += is_leap_year(year) ? 366:365;
+        }
+
+        for(int month = 0; month <hexToDecimal(time[5])-1; month++)
+        {
+            total_days += days_in_month[month];
+            if(month ==1 && is_leap_year(hexToDecimal(time[6])+ 1900))
+            {
+                total_days++;
+            }
+        }
+        total_days += hexToDecimal(time[4])-1;
+
+        timestamp_segs = total_days * 86400 + hexToDecimal(time[2])*3600 + hexToDecimal(time[1])*60 + hexToDecimal(time[0]);
+        timestamp_segs =hexToDecimal(time[2])*3600 + hexToDecimal(time[1])*60 + hexToDecimal(time[0]);
+        
+        sprintf(timestamp, "%d", timestamp_segs);
     }
+
+    //printf("timeStamp: %d\n", timestamp_segs);
+    //printf("timeStamp: %s\n", timestamp);
 
     return ret;
 }
